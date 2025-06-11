6bd14b4241506717f1be9d785f0a938f69cb5765
Kirill1018 <kirillfedorov031@yandex.ru>
2025-06-11 18:41:28 +0300

    image

--- app/src/main/java/com/example/art/data/ArtApi.kt
+++ app/src/main/java/com/example/art/data/ArtApi.kt
@@ -7,6 +7,6 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ArtApi {
-    @GET("artworks")
-    fun getArt(@Query("limit") end: Int, @Query("page") webPage: Int): Call<Artwork>
+    @GET("collection")
+    fun getArt(@Query("key") apiKey: String): Call<Artwork>
 }
